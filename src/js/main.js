8cebac623c450ab1164972627c16b61e1552deb5
abl69 <ab7@bk.ru>
2019-10-24 19:25:46 +0300

    подключена библиотека jQyery и сделано подключение модального окна

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,18 @@ var button = document.querySelector('#button');
 var modal = document.querySelector('#modal');
 var close = document.querySelector('#close');
 
+function modalClose() {
+	modal.classList.remove('modal-active');
+}
+
 button.addEventListener ('click', function() {
-	modal.classList.add('modal-active')
+	modal.classList.add('modal-active');
+	setTimeout(modalClose, 5000);
 });
 
-close.addEventListener ('click', function() {
-	modal.classList.remove('modal-active')
-});
+/*close.addEventListener ('click', function() {
+	modal.classList.remove('modal-active');
+});*/
+
+
+
